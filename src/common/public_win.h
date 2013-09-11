05749f1e5a41ebecec904e331c294e2b8f1e6f27
zhanleewo <zhanleewo@gmail.com>
2013-09-11 18:01:50 +0800

    initial version

--- src/common/public_win.h
+++ src/common/public_win.h
@@ -1,17 +1,5 @@
 #ifndef COMMON_PUBLIC_WIN
 #define COMMON_PUBLIC_WIN
-
-#include <stdio.h>
-#include <uninstd.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <uuid.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
 #include <string>
 #include <list>
 #include <map>
